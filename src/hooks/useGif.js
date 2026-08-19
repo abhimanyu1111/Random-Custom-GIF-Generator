55e8c0a9d45f1d09c2592f601ac57147764a3c79
abhimanyu1111 <abhimanyukumar6531@gmail.com>
2026-08-20 00:02:29 +0530

    Removed unused useEffect

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
